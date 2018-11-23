257dcf8d13c7339833d4c48fe5d9c1860b4273bf
VVoitenko <viktorvoitenkov@gmail.com>
2018-11-23 18:27:32 +0200

    Initial structure

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">888888
+      <div className="App">888888999
         <header className="App-header">fff
           <img src={logo} className="App-logo" alt="logo" />
           <p>
