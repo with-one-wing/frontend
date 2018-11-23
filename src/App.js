238c1596955d8cb09f16a68a41a42f065897623a
VVoitenko <viktorvoitenkov@gmail.com>
2018-11-23 18:12:26 +0200

    Initial structure

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">888888
-        <header className="App-header">
+        <header className="App-header">fff
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
