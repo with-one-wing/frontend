f64027923cb0710585e5208f42f84fdd35cc9cb2
VVoitenko <viktorvoitenkov@gmail.com>
2018-11-27 12:07:26 +0200

    Check hook

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">888888999
+      <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
