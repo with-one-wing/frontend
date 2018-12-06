01b8d808551f830700ed0baf764997d68f18a60e
with-one-wing <viktor.voitenko@gmail.com>
2018-12-06 18:43:29 +0200

    asdas
    asd

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">111
+        <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             <code>src/App.js</code> and save to reload.
