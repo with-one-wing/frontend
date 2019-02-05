c9a1d6d08c32c30a5e07ac55ea7774458b17b218
vvoitenko <vvoitenko@luxoft.com>
2019-02-05 14:47:52 +0200

    push

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn Reactssss
+            Learn REACT. Test deploy
           </a>
         </header>
       </div>
